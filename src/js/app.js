8d28154a77b07ccccf46a68127cbe2846f4240db
arturSantoro <artur.santoro@sienacompany.com>
2023-10-25 19:20:24 -0300

    Iniciando Back-End

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        msg:"Hello World",
+    })
+});
+
+app.listen(port);
